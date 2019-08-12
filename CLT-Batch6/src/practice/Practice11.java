ca415865976c2b0293565a54613e7014b3b82e85
Iman <imanhamzah111@gmail.com>
2019-08-13 07:26:07 +0800

    13 Aug 2019

--- CLT-Batch6/src/practice/Practice11.java
+++ CLT-Batch6/src/practice/Practice11.java
@@ -9,11 +9,28 @@ public class Practice11 {
 	int sum(int x, int y, int z) {
 		return (x + y + z);
 	}
+	
+	int add() {
+		int a = 10;
+		int b = 8;
+		int c = a + b;
+		return c;
+		
+	}
+	
+	void addWithPrint() {
+		int a = 2;
+		int b = 4;
+		int c = a + b;
+		System.out.println(c);
+	}
 
 	public static void main(String[] args) {
 		
 		Practice11 prac = new Practice11();
 		System.out.println(prac.sum(2,5,8));
+		System.out.println(prac.add());
+		prac.addWithPrint();
 
 	}
 
